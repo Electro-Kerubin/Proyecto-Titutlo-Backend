43bfd02a7eaca55f1b037d0834693cf9d418b6fb
Rodrigo Baeza <rodrigo.baeza@impact.cl>
2023-07-23 15:06:59 -0400

    cambios en las rutas para agregar el https

--- src/main/java/com/proyectotitulo/springbootproyectotitulo/controlador/ResenaControlador.java
+++ src/main/java/com/proyectotitulo/springbootproyectotitulo/controlador/ResenaControlador.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.Table;
 
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://localhost:3000")
 @RestController
 @RequestMapping("/api/resenas")
 public class ResenaControlador {
